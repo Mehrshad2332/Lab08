40ac96206c2107910121bfec90445adecf0a5a48
Mehrshad <m.talebzadeh82@gmail.com>
2026-03-06 15:17:09 -0700

    TEST testDeleteCity FAILED.MTZ

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,7 +1,27 @@
 package com.example.lab08;
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomListTest {
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+
+        assertTrue(list.hasCity(calgary));
+    }
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+
+        City edmonton = new City("Edmonton", "Canada");
+        list.addCity(edmonton);
+
+        list.deleteCity(edmonton);
+
+        assertFalse(list.hasCity(edmonton));
+    }
 
 }
